638f3e953f0f1f9ff03ae1dc2acb3aa79d2e4cf0
Nitin Gupta <cmnitinedugupta@gmail.com>
2025-05-23 22:10:35 +0530

    guncorn

--- server/gunicorn.conf.py
+++ server/gunicorn.conf.py
@@ -1,10 +1,11 @@
 import os
 import multiprocessing
 
-workers = multiprocessing.cpu_count() * 2 + 1
+# Use a fixed number of workers for Render free tier
+workers = 2  # Limited workers for free tier
 timeout = 120
 bind = "0.0.0.0:10000"
-worker_class = "gevent"
+worker_class = "sync"
 preload_app = True
 
 # Log settings
